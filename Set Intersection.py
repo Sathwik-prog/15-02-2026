1756e9936f4d7bfaa2c23dc2c77201ac72c47e9f
Sathwik-prog <bondalapatiradhika1989@gmail.com>
2026-02-15 16:37:05 +0530

    ez

---
+++ Set Intersection.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("Orignal set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of setx and sety:")
+setz = setx.intersection(sety)
+print(setz)
